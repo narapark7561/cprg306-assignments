9ba001483036a92a202188876c67a17fb952dd71
narapark7561 <narapark7561@gmail.com>
2024-09-12 20:39:17 -0600

    Edited Root Page by add heading and link to week-2 page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="./week-2">Click to go week-2 page!</Link>
+    </main>
+  );
 }
