6e1f4e66a91e319b5649717d44bd2825f6f4da70
narapark7561 <narapark7561@gmail.com>
2024-09-29 23:33:31 -0600

    Completed Assignment 4

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -19,11 +19,28 @@ export default function NewItem() {
     }
   };
 
+  let incrementButtonStyles = "bg-blue-500 text-white rounded-md py-0.5 px-4";
+  let decrementButtonStyles = "bg-gray-400 text-white rounded-md py-0.5 px-4";
+
+  if (quantity === 20) {
+    incrementButtonStyles = "bg-gray-400 text-white rounded-md py-0.5 px-4";
+    decrementButtonStyles = "bg-blue-500 text-white rounded-md py-0.5 px-4";
+  } else if (quantity === 1) {
+    decrementButtonStyles = "bg-gray-400 text-white rounded-md py-0.5 px-4";
+  } else {
+    incrementButtonStyles = "bg-blue-500 text-white rounded-md py-0.5 px-4";
+    decrementButtonStyles = "bg-blue-500 text-white rounded-md py-0.5 px-4";
+  }
+
   return (
-    <div>
-      <p>{quantity}</p>
-      <button onClick={decrement}>-</button>
-      <button onClick={increment}>+</button>
+    <div className="bg-white p-4 rounded-lg flex justify-center space-x-2 max-w-60 mt-10">
+      <p className="font-semibold mr-7">{quantity}</p>
+      <button onClick={decrement} className={decrementButtonStyles}>
+        -
+      </button>
+      <button onClick={increment} className={incrementButtonStyles}>
+        +
+      </button>
     </div>
   );
 }
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -2,7 +2,7 @@ import NewItem from "./new-item";
 
 export default function NewItemPage() {
   return (
-    <main>
+    <main className="bg-black min-h-screen flex flex-col justify-start items-center">
       <NewItem />
     </main>
   );
