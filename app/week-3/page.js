8012562cff7c5e660d05a4424057762aa8919dd3
narapark7561 <narapark7561@gmail.com>
2024-09-29 23:42:15 -0600

    Completed Assignment 3

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <main className="p-10 bg-gray-900 ">
       <h1 className="text-3xl font-bold text-white text-left mb-8">
-        Shopping List
+        Shopping List ☑️
       </h1>
       <ItemList />
     </main>
